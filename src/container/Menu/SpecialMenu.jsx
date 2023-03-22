99822269b1846967c81e03eba935b61436858762
unknown <tomygarnero9429@gmail.com>
2023-03-22 17:16:07 -0300

    SpecialMenu component added

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -12,7 +12,7 @@ const SpecialMenu = () => (
         </div>
         <div className='app__specialMenu-menu'>
             <div className='app__specialMenu-menu_wine flex__center'>
-                <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+                <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
                 <div className='app__specialMenu_menu_items'>
                     {data.wines.map((wine, index) => (
                         <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
@@ -23,7 +23,7 @@ const SpecialMenu = () => (
                 <img src={images.menu} alt="menu img" />
             </div>
             <div className='app__specialMenu-menu_cocktails flex__center'>
-                <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+                <p className='app__specialMenu-menu_heading'>Cocktails</p>
                 <div className='app__specialMenu_menu_items'>
                     {data.cocktails.map((cocktail, index) => (
                         <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
